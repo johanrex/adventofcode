acdec914d2e0a63b3631595049151b553c5311cf
Johan Rex <52hands@gmail.com>
2021-12-09 17:10:12 +0100

    9

--- 2021/9.py
+++ 2021/9.py
@@ -1,6 +1,5 @@
 import numpy as np
-import scipy
-from scipy.signal import argrelextrema
+import common
 
 def read_input(filename):
     inputs = []
@@ -12,30 +11,86 @@ def read_input(filename):
     mtx = np.asarray(inputs, np.int8)
     return mtx
 
+def get_smallest_points(mtx):
+    smallest_points = []
+    rows, cols = mtx.shape
+    for row in range(1, rows-1):
+        for col in range(1, cols-1):
+
+            val = mtx[row,col]
+
+            #is smaller than adjacent?
+            if (
+                val < mtx[row-1,col] and 
+                val < mtx[row+1,col] and 
+                val < mtx[row,col-1] and 
+                val < mtx[row,col+1]
+            ):
+                smallest_points.append( (row,col) )
+
+    return smallest_points
+
+def flood_fill(row, col, mtx):
+
+    nr_of_rows = mtx.shape[0]
+    nr_of_rows = mtx.shape[1]
+
+    points_affected = 0
+
+    if mtx[row,col] != -1 and mtx[row,col] < 9:
+
+        #use -1 to indicate visited
+        mtx[row,col] = -1
+        points_affected = 1
+
+        if row > 0:
+            points_affected += flood_fill(row-1,col, mtx)
+        if row < nr_of_rows - 1:
+            points_affected += flood_fill(row+1,col, mtx)
+        if col > 0:
+            points_affected += flood_fill(row,col-1, mtx)
+        if row < nr_of_rows -1:
+            points_affected += flood_fill(row,col+1, mtx)
+
+    return points_affected
+
+def part1(smallest_points, mtx):
+    s = 0
+
+    #TODO how to extract elements from array without loop?
+    for point in smallest_points:
+        s += mtx[point]+1
+
+    print(f'Part 1. Sum: {s}.')
+
+def part2(smallest_points, mtx):
+
+    basin_sizes = []
+    for low_point in smallest_points:
+        row = low_point[0]
+        col = low_point[1]
+        size = flood_fill(row,col, mtx)
+        basin_sizes.append(size)
+
+    s = np.prod(sorted(basin_sizes, reverse=True)[:3])
+    print(f'Part 2. Sum: {s}.')
+
+@common.timed
+def challenge(filename):
+
+    mtx = read_input(filename)
+
+    #pad with bigger value outside for gloriously easy manipulation. 
+    mtx = np.pad(mtx, 1, constant_values=10)
+
+    #common input to both parts
+    smallest_points = get_smallest_points(mtx)
+
+
+    part1(smallest_points, mtx)
+    part2(smallest_points, mtx)
+
 #filename = '2021/9_input_example.txt'
 filename = '2021/9_input.txt'
 
-mtx = read_input(filename)
-
-#pad with bigger value outside
-mtx = np.pad(mtx, 1, constant_values=10)
-
-
-smallest = []
-rows, cols = mtx.shape
-for x in range(1, rows-1):
-    for y in range(1, cols-1):
-
-        val = mtx[x,y]
-
-        #is smaller than adjacent?
-        if (
-            val < mtx[x-1,y] and 
-            val < mtx[x+1,y] and 
-            val < mtx[x,y-1] and 
-            val < mtx[x,y+1]
-        ):
-            smallest.append(val)
-
-sum = (np.array(smallest)+1).sum()
-print(f'Part 1. Sum: {sum}.')
+challenge(filename)
