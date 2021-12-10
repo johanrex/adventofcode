4c1a6606455e044580889896b9833d8b77ecb207
Johan Rex <52hands@gmail.com>
2021-12-10 16:32:24 +0100

    10

--- 2021/10.py
+++ 2021/10.py
@@ -1,76 +1,71 @@
 from collections import deque
 
-def read_input(filename):
-    lines = []
-    with(open(filename, "r")) as f:
-        for line in f:
-            lines.append(list(line.strip()))
-    
-    return lines
-
-lines = read_input('2021/10_input_example.txt')
-lines = read_input('2021/10_input.txt')
+#filename = '2021/10_input_example.txt'
+filename = '2021/10_input.txt'
 
 syntax_error_score = 0
 completion_scores = []
 
-for line in lines:
-    stack = deque()
-    corrupted = False
+with(open(filename, "r")) as f:
+    for line in f:
+        line = line.strip()
 
-    for c in line:
-        if c in "([{<":
-            stack.append(c)
-        elif c in ")]}>":
-            popped = stack.pop()
-            if (
-                (popped == '(' and c == ')') or
-                (popped == '[' and c == ']') or
-                (popped == '{' and c == '}') or
-                (popped == '<' and c == '>')
-                ):
-                pass
+        stack = deque()
+        corrupted = False
+
+        for c in line:
+            if c in "([{<":
+                stack.append(c)
+            elif c in ")]}>":
+                popped = stack.pop()
+                if (
+                    (popped == '(' and c == ')') or
+                    (popped == '[' and c == ']') or
+                    (popped == '{' and c == '}') or
+                    (popped == '<' and c == '>')
+                    ):
+                    pass
+                else:
+                    corrupted = True
+
+                    match c:
+                        case ')':
+                            syntax_error_score += 3
+                        case ']':
+                            syntax_error_score += 57
+                        case '}':
+                            syntax_error_score += 1197
+                        case '>':
+                            syntax_error_score += 25137
+                        case _:
+                            raise Exception('unexpected')
+                    break
             else:
-                corrupted = True
+                raise Exception('unexpected')
+
+        if not corrupted:
+            #is incomplete
+            stack.reverse()
+            s = ''.join(stack)
+            s = s.replace('(', ')').replace('[', ']').replace('{', '}').replace('<', '>')
+
+            completion_score = 0
+            for c in s:
+                completion_score *= 5
 
                 match c:
                     case ')':
-                        syntax_error_score += 3
+                        completion_score += 1
                     case ']':
-                        syntax_error_score += 57
+                        completion_score += 2
                     case '}':
-                        syntax_error_score += 1197
+                        completion_score += 3
                     case '>':
-                        syntax_error_score += 25137
+                        completion_score += 4
                     case _:
-                        raise Exception('unexpected')
-                break
-        else:
-            raise Exception('unexpected')
+                        raise Exception('unexpected')            
 
-    if not corrupted:
-        #is incomplete
-        stack.reverse()
-        s = ''.join(stack)
-        s = s.replace('(', ')').replace('[', ']').replace('{', '}').replace('<', '>')
-
-        completion_score = 0
-        for c in s:
-            completion_score *= 5
-
-            match c:
-                case ')':
-                    completion_score += 1
-                case ']':
-                    completion_score += 2
-                case '}':
-                    completion_score += 3
-                case '>':
-                    completion_score += 4
-                case _:
-                    raise Exception('unexpected')            
-
-        completion_scores.append(completion_score)
+            completion_scores.append(completion_score)
 
 completion_scores = sorted(completion_scores)
 
