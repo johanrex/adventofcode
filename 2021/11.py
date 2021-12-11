1c725408e473715df6efa6410d9f1571c91ad679
Johan Rex <52hands@gmail.com>
2021-12-11 14:52:28 +0100

    11

--- 2021/11.py
+++ 2021/11.py
@@ -3,13 +3,11 @@ from timeit import default_timer as timer
 t1 = timer()
 
 def read_input(filename):
-    inputs = []
+    lines = []
     with(open(filename, "r")) as f:
-        for line in f:
-            row = list(line.strip())
-            inputs.append(row)
+        lines = [ list(line.strip()) for line in f.readlines() ]
 
-    m = np.asarray(inputs, np.int8)
+    m = np.asarray(lines, np.int8)
     return m
 
 def print_mtx(m):
@@ -17,39 +15,39 @@ def print_mtx(m):
     for row in m:
         print(''.join(row))
 
-def flash(m):
+def step(m):
 
-    flashers = np.where(m >= 10)
-    m[m >= 10] = 0
+    nr_of_flashes = 0
 
-    global nr_of_flashes
-    nr_of_flashes += len(flashers[0])
+    while True:
+        flashers = np.where(m >= 10)
+        m[m >= 10] = 0
 
-    points_affected = []
+        nr_of_flashes += flashers[0].size
 
-    for row,col in zip(flashers[0], flashers[1]):
+        for row,col in zip(flashers[0], flashers[1]):
 
-        for pt in (
-            (row-1, col-1),
-            (row-1, col),
-            (row-1, col+1),
-            (row, col-1),
-            (row, col+1),
-            (row+1, col-1),
-            (row+1, col),
-            (row+1, col+1)
-        ):
-            #Skip points outside matrix and those already flashed.             
-            if (
-                (0 <= pt[0] < m.shape[0]) and 
-                (0 <= pt[1] < m.shape[1]) and 
-                (m[pt[0], pt[1]] != 0)
+            for pt in (
+                (row-1, col-1),
+                (row-1, col),
+                (row-1, col+1),
+                (row, col-1),
+                (row, col+1),
+                (row+1, col-1),
+                (row+1, col),
+                (row+1, col+1)
             ):
-                #+1 for all affected
-                m[ pt[0], pt[1] ] = m[ pt[0], pt[1] ] + 1
-    
-    if np.any(m >= 10):
-        flash(m)
+                #Skip points outside matrix and those already flashed.             
+                if (
+                    (0 <= pt[0] < m.shape[0]) and 
+                    (0 <= pt[1] < m.shape[1]) and 
+                    (m[pt[0], pt[1]] != 0)
+                ):
+                    #+1 for all affected
+                    m[ pt[0], pt[1] ] = m[ pt[0], pt[1] ] + 1
+        
+        if not np.any(m >= 10):
+            return nr_of_flashes
 
 filename = '2021/11_input.txt'
 m = read_input(filename)
@@ -64,15 +62,16 @@ step_when_all_flash = 0
 
 #Steps
 for i in range(1000):
-    step = i+1
+    step_nbr = i+1
     m = m + 1
 
-    flash(m)
-    if step == 100:
+    nr_of_flashes = step(m)
+    if step_nbr == 100:
         part1_nr_of_flashes = nr_of_flashes
 
-    if not np.any(m > 0):
-        step_when_all_flash = step
+    if not np.any(m):
+    #if nr_of_flashes == (m.shape[0] * m.shape[1]):
+        step_when_all_flash = step_nbr
         break
 
     # print(f'After step {step}:')
