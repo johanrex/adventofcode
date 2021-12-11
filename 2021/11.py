029197fc244307c5a8edf84d245f4836c1587549
Johan Rex <52hands@gmail.com>
2021-12-11 18:52:49 +0100

    11 numpy

--- 2021/11.py
+++ 2021/11.py
@@ -18,36 +18,63 @@ def print_mtx(m):
 def step(m):
 
     nr_of_flashes = 0
+    rolls = []
+
+    #keep track of those that is already zero in this step
+    mbz = m==0
 
     while True:
-        flashers = np.where(m >= 10)
-        m[m >= 10] = 0
+        mb = m>=10
+        mf = mb.astype(np.int8)
 
-        nr_of_flashes += flashers[0].size
+        s = mf.sum()
 
-        for row,col in zip(flashers[0], flashers[1]):
+        if s>0:
+            nr_of_flashes += s
 
-            for pt in (
-                (row-1, col-1),
-                (row-1, col),
-                (row-1, col+1),
-                (row, col-1),
-                (row, col+1),
-                (row+1, col-1),
-                (row+1, col),
-                (row+1, col+1)
-            ):
-                #Skip points outside matrix and those already flashed.             
-                if (
-                    (0 <= pt[0] < m.shape[0]) and 
-                    (0 <= pt[1] < m.shape[1]) and 
-                    (m[pt[0], pt[1]] != 0)
-                ):
-                    #+1 for all affected
-                    m[ pt[0], pt[1] ] = m[ pt[0], pt[1] ] + 1
-        
-        if not np.any(m >= 10):
-            return nr_of_flashes
+            #Pad with 0
+            mfp = np.pad(mf, pad_width=1, mode='constant', constant_values=0)
+
+            """
+            345
+            2x6
+            107
+            """
+
+            rolls.clear()
+            rolls.append(np.roll(mfp, 1, 0)) #0
+            rolls.append(np.roll(rolls[0], -1, 1)) #1
+            rolls.append(np.roll(rolls[1], -1, 0)) #2
+            rolls.append(np.roll(rolls[2], -1, 0)) #3
+            rolls.append(np.roll(rolls[3], 1, 1)) #4
+            rolls.append(np.roll(rolls[4], 1, 1)) #5
+            rolls.append(np.roll(rolls[5], 1, 0)) #6
+            rolls.append(np.roll(rolls[6], 1, 0)) #7
+
+            madj = \
+                rolls[0] + \
+                rolls[1] + \
+                rolls[2] + \
+                rolls[3] + \
+                rolls[4] + \
+                rolls[5] + \
+                rolls[6] + \
+                rolls[7]
+
+            #Remove padding
+            madj = madj[1:-1, 1:-1]
+
+            m += madj
+
+            mbz |= mb
+
+            #set all flashed to 0
+            m[mbz] = 0
+           
+        else:
+            break
+
+    return nr_of_flashes
 
 filename = '2021/11_input.txt'
 m = read_input(filename)
@@ -61,20 +88,19 @@ step_when_all_flash = 0
 # print('')
 
 #Steps
-for i in range(1000):
+for i in range(500):
     step_nbr = i+1
-    m = m + 1
+    m += 1
 
-    nr_of_flashes = step(m)
+    nr_of_flashes += step(m)
     if step_nbr == 100:
         part1_nr_of_flashes = nr_of_flashes
 
     if not np.any(m):
-    #if nr_of_flashes == (m.shape[0] * m.shape[1]):
         step_when_all_flash = step_nbr
         break
 
-    # print(f'After step {step}:')
+    # print(f'After step {step_nbr}:')
     # print_mtx(m)
     # print('')
 i = 0
