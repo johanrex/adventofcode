505151f31541d7e25f3112f8f61a158e7805c06a
Johan Rex <52hands@gmail.com>
2021-12-12 14:03:53 +0100

    12

--- 2021/12.py
+++ 2021/12.py
@@ -1,12 +1,8 @@
-
-#input = edge list.
-
-#detect is small cyclic
 import copy
 import pprint
 from collections import defaultdict
-
-filename = '2021/12_input_example.txt'
+from timeit import default_timer as timer
+t1 = timer()
 
 class Graph(object):
 
@@ -18,33 +14,15 @@ class Graph(object):
         self._graph[node1].add(node2)
         self._graph[node2].add(node1)
 
-    # def is_connected(self, node1, node2):
-    #     return node1 in self._graph and node2 in self._graph[node1]
-
-    # def find_path(self, node1, node2, path=[]):
-    #     """ Find any path between node1 and node2 (may not be shortest) """
-
-    #     path = path + [node1]
-    #     if node1 == node2:
-    #         return path
-    #     if node1 not in self._graph:
-    #         return None
-    #     for node in self._graph[node1]:
-    #         if node not in path:
-    #             new_path = self.find_path(node, node2, path)
-    #             if new_path:
-    #                 return new_path
-    #     return None
-
-    def find_all_paths(self, node1, node2, path = []):
+    def find_all_paths(self, traverse_option, node1, node2, path = []):
         paths=[]
         path=[]
 
-        self.__find_all_paths(node1, node2, path, paths)
+        self.__find_all_paths(traverse_option, node1, node2, path, paths)
 
         return paths        
 
-    def __find_all_paths(self, node1, node2, path, paths):
+    def __find_all_paths(self, traverse_option, node1, node2, path, paths):
 
         path = path + [node1]
         if node1 == node2:
@@ -53,18 +31,33 @@ class Graph(object):
         if node1 not in self._graph:
             return None
         for node in self._graph[node1]:
-            if node.islower() and node not in path:
-                self.__find_all_paths(node, node2, path, paths)
-            elif node.isupper() and node != path[len(path)-1]:
 
-                self.__find_all_paths(node, node2, path, paths)
+            if traverse_option == 1:
+                if node.islower() and node not in path:
+                    self.__find_all_paths(traverse_option, node, node2, path, paths)
+                elif node.isupper() and node != path[len(path)-1]:
+                    self.__find_all_paths(traverse_option, node, node2, path, paths)
+            else:
+                if node.islower() and node != 'start':
+
+                    if node not in path:
+                        self.__find_all_paths(traverse_option, node, node2, path, paths)
+                    else:
+                        #count, groupby lower
+                        lower_count = [(x,path.count(x)) for x in set(path) if x.islower()]
+                        any_more_than_1 =  any(tpl[1][1] > 1 for tpl in enumerate(lower_count))
+
+                        if not any_more_than_1:
+                            self.__find_all_paths(traverse_option, node, node2, path, paths)
+
+                elif node.isupper() and node != path[len(path)-1]:
+                    self.__find_all_paths(traverse_option, node, node2, path, paths)
 
         return None        
 
-    # def __str__(self):
-    #     return '{}({})'.format(self.__class__.__name__, dict(self._graph))
-
 g = Graph()
+
+filename = '2021/12_input.txt'
 
 #read input
 with(open(filename, "r")) as f:
@@ -75,10 +68,11 @@ for line in lines:
 pretty_print = pprint.PrettyPrinter()
 pretty_print.pprint(g._graph)
 
-lst = g.find_all_paths('start', 'end')
-for path in lst:
-    p = ','.join(path)
-    print(p)
-
+lst = g.find_all_paths(1, 'start', 'end')
 print('Part 1. Nr of paths:', len(lst))
-i = 0
+
+lst = g.find_all_paths(2, 'start', 'end')
+print('Part 2. Nr of paths:', len(lst))
+
+t2 = timer()
+print(f'time: {(t2-t1):.4f}s')
