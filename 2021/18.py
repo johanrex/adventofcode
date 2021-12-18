cec5d0d1374089e0923f458c87c0358b8c29703d
Johan Rex <52hands@gmail.com>
2021-12-18 22:27:39 +0100

    18

--- 2021/18.py
+++ 2021/18.py
@@ -11,23 +11,30 @@ class Pair:
         self.right = None
         self.parent = None
 
-    def __str__(self):
-        if type(self.left) == Pair:
-            str_left = str(self.left)
-        else:
-            str_left = self.left
+    def has_left_int(self) -> bool:
+        return type(self.left) == int
 
-        if type(self.right) == Pair:
-            str_right = str(self.right)
-        else:
-            str_right = self.right
+    def has_right_int(self) -> bool:
+        return type(self.right) == int
+
+    def has_left_child(self) -> bool:
+        return not self.has_left_int()
+
+    def has_right_child(self) -> bool:
+        return not self.has_right_int()
+
+    def __str__(self):
+        str_left = str(self.left)
+        str_right = str(self.right)
 
         s = f'[{str_left},{str_right}]'
 
         return s
 
-
 def parse_line(s):
+
+    print('parsing line:', s)
+    
     global parse_line_idx
     parse_line_idx = 0 
     p = __parse_pair(s)
@@ -89,22 +96,18 @@ def find_exploder(p, depth = 0):
     ret = None
 
     if depth == 4:
-        if (
-            type(p.left) == int and 
-            type(p.right) == int
-            ):
+        if p.has_left_int() and p.has_right_int():
             ret = p
     elif depth < 4:
-        if type(p.left) == Pair:
+        if p.has_left_child():
             ret = find_exploder(p.left, depth+1)
         
-        if ret == None and type(p.right) == Pair:
+        if ret == None and p.has_right_child():
                 ret = find_exploder(p.right, depth+1)
 
     return ret
 
 def find_first_number_to_the_left(p):
-    pass
     p.parent
 
 
@@ -130,53 +133,23 @@ def reduce(p):
 
     return p
 
-# def populate_nx_graph(g,p):
-
-#     if p != None:
-
-#         node_id = len(g)
-
-#         g.add_node(node_id)
-
-#         vals = {}
-
-#         #Left
-#         if type(p.left) == Pair:
-#             child_id = populate_nx_graph(g, p.left)
-#             g.add_edge(node_id, child_id, side='left')
-#         else:
-#             vals['left_val'] = p.left
-#             g.nodes[node_id]['left_val'] = p.left
-            
-
-#         #Right
-#         if type(p.right) == Pair:
-#             child = populate_nx_graph(g, p.right)
-#             g.add_edge(node_id, child_id, side='right')
-#         else:
-#             vals['right_val'] = p.right
-#             g.nodes[node_id]['right_val'] = p.right
-
-#         return node_id
-
-
 def print_tree(node, level=0):
     if node != None:
 
-        if type(node.right) == Pair:
+        if node.has_right_child():
             print_tree(node.right, level + 1)
 
         left_val = '_'
-        if type(node.left) == int:
+        if node.has_left_int():
             left_val = node.left
 
         right_val = '_'
-        if type(node.right) == int:
+        if node.has_right_int():
             right_val = node.right
 
         print(' ' * 4 * level + '->', f'({left_val},{right_val})')
 
-        if type(node.left) == Pair:
+        if node.has_left_child():
             print_tree(node.left, level + 1)
 
 
@@ -184,37 +157,19 @@ def show_tree(p):
 
     print_tree(p)
 
-    # import networkx as nx
-
-    # g = nx.DiGraph()
-    # populate_nx_graph(g, p)
-
-
-    # import matplotlib.pyplot as plt
-    # pos = hierarchy_pos(g,0)    
-    # nx.draw(g, pos=pos, with_labels=True)
-    # plt.savefig('hierarchy.png')
-
-
-    # pos = graphviz_layout(g, prog="dot")
-    # nx.draw(g, pos)
-    # plt.show()
-
-
-
 
 # filename = '2021/18_input_example.txt'
 # with open(filename) as f:
 #     lines = [line.strip() for line in f]
 
-#verify('[1,2]')
-verify('[[1,2],3]')
-verify('[9,[8,7]]')
-verify('[[1,9],[8,5]]')
-verify('[[[[1,2],[3,4]],[[5,6],[7,8]]],9]')
-verify('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]')
-verify('[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]')
-verify('[[[[[9,8],1],2],3],4]')
+# verify('[1,2]')
+# verify('[[1,2],3]')
+# verify('[9,[8,7]]')
+# verify('[[1,9],[8,5]]')
+# verify('[[[[1,2],[3,4]],[[5,6],[7,8]]],9]')
+# verify('[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]')
+# verify('[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]')
+# verify('[[[[[9,8],1],2],3],4]')
 
 show_tree((parse_line('[[[[[9,8],1],2],3],4]')))
 
