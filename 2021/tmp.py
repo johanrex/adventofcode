de655465a55ea22375129859ebbfbe48663bd082
Johan Rex <52hands@gmail.com>
2022-01-01 14:31:57 +0100

    23

--- 2021/tmp.py
+++ 2021/tmp.py
@@ -1,9 +1,13 @@
+#TODO explicit type aliases https://www.python.org/dev/peps/pep-0613/
+
 from __future__ import annotations
 from typing import List
 from dataclasses import dataclass
 import copy
 import networkx as nx
 import itertools
+
+ROOM_SLOTS = 2
 
 A = 1
 B = 2
@@ -17,6 +21,21 @@ mapper = {
     'D' : D
 }
 
+room_to_apod_mapper = {
+    3: A,
+    5: B,
+    7: C,
+    9: D
+}
+
+apod_to_room_mapper = {
+    A: 3,
+    B: 5,
+    C: 7,
+    D: 9
+}
+
+
 def create_graph() -> nx.Graph:
     G = nx.Graph()
 
@@ -27,7 +46,7 @@ def create_graph() -> nx.Graph:
         if col > 0:
             G.add_edge( (row, col), (row, col-1) )
 
-    for row in range(2, 4):
+    for row in range(2, 2 + ROOM_SLOTS):
         for col in [3,5,7,9]:
             G.add_node( (row, col) )
             G.add_edge( (row, col), (row-1, col) )
@@ -38,7 +57,7 @@ def create_graph() -> nx.Graph:
 def get_start_state(lines):
     pos_apods = {}
 
-    for row in range(2, 4):
+    for row in range(2, 2 + ROOM_SLOTS):
         for col in [3,5,7,9]:
             pos_apods[(row, col)] = mapper[lines[row][col]]
 
@@ -62,8 +81,8 @@ def read_input(filename):
 
     return lines 
 
-def is_node_occupied(state: dict, pos: tuple[int, int]) -> bool:
-    return pos in state
+def is_free_pos(state: dict, pos: tuple[int, int]) -> bool:
+    return pos not in state
 
 def is_no_parking(pos: tuple[int, int]):
     return pos in [(1,3), (1, 5), (1,7), (1,9)]
@@ -71,22 +90,77 @@ def is_no_parking(pos: tuple[int, int]):
 def is_hallway(pos: tuple(int, int)) -> bool:
     return pos[0] == 1
 
-def get_reachable_vertices(G: nx.Graph, state: dict[tuple[int, int], int], source: tuple[int, int] ):
+def is_free_path(G: nx.Graph, current_state: dict[tuple[int, int], int], src: tuple[int, int], dst: tuple[int, int]):
 
-    paths = list(nx.dfs_edges(G, source=source))
+    paths = nx.shortest_simple_paths(G, src, dst)
+    shortest_path = next(paths)
 
-    for path in paths:
-        print(path)
-
-        #Prune paths according to:
-        #is_no_parking()
-        #is_node_occupied()
-        # is allowed room
-        # move from room to hallway. (don't get stuck moving up and down in room)
+    found_other_apod_along_path = next(True for pos in shortest_path if pos in current_state, False)
+    return not found_other_apod_along_path
 
 
-def organize(G):
-    pass
+def get_valid_room_pos(current_state: dict[tuple[int, int], int], apod: int, current_pos: tuple[int, int]):
+    dst_room_col =apod_to_room_mapper[apod]
+
+    current_col = current_pos[1]
+
+    assert current_col != dst_room_col
+
+    room_coordinates = [(depth, dst_room_col) for depth in range(2, 2+ROOM_SLOTS)]
+
+    is_other_apod_in_room = next(True for room_coordinate in room_coordinates if room_coordinate in current_state, False)
+
+    if is_other_apod_in_room:
+        return False
+
+    first_free_pos_from_bottom = next(reversed(room_coordinate for room_coordinate in room_coordinates if room_coordinate not in current_state), None)
+    if first_free_pos_from_bottom is not None:
+        return first_free_pos_from_bottom
+
+
+def get_valid_hallway_positions(G: nx.Graph, current_state: dict[tuple[int, int], int], start_pos: tuple[int, int]):
+
+    positions = []
+
+    leftmost_col = 1
+    rightmost_col = 11
+
+    for col in 
+
+    leftmost_hallway_pos = (1, 1)
+    rightmost_hallway_pos = (1, 11)
+
+    paths = nx.shortest_simple_paths(G, start_pos, leftmost_hallway_pos)
+    
+    for pos in paths:
+        if not is_free_pos(pos):
+            break 
+        
+        if is_no_parking(pos):
+            continue
+
+        positions.append(pos)
+
+
+def get_valid_moves(apod, pos, current_state):
+    moves = []
+
+    if is_hallway(pos):
+        dst_room =apod_to_room_mapper[apod]
+        valid_room_pos = get_valid_room_pos(current_state, apod, pos)
+        if valid_room_pos is not None:
+            moves.append(valid_room_pos)
+    else:
+        is_free_path        
+
+    return moves
+
+
+def sort(G, current_state, end_state):
+    for pos in current_state:
+        apod = current_state[pos]
+        moves = get_valid_moves(apod, pos, current_state)
+
 
 filename = '2021/23_input_example.txt'
 lines = read_input(filename)
@@ -94,9 +168,12 @@ lines = read_input(filename)
 start_state = get_start_state(lines)
 end_state = get_end_state(start_state)
 
-
 G = create_graph()
-get_reachable_vertices(G, start_state, (2, 3) )
+
+is_free_path(G, start_state, (3,3), (1, 1) )
+
+sort(G, start_state, end_state)
+#get_reachable_vertices(G, start_state, (2, 3) )
 
 
 import pickle
