b656d138578a30991e0c315eed9add2103d06516
Johan Rex <52hands@gmail.com>
2022-07-28 15:15:52 +0200

    ...

--- 2021/d24.py
+++ 2021/d24.py
@@ -10,13 +10,12 @@ def get_initial_state() -> dict:
     }
 
 
-def evaluate_lines(lines: list[str], model_nr: int):
+def evaluate_inputs(tokenized_inputs: list[list[str]], model_nr: int):
     state = get_initial_state()
     model_nr_str = str(model_nr)
     inp_offset = 0
 
-    for line in lines:
-        tokens = line.split()
+    for tokens in tokenized_inputs:
 
         if len(tokens) == 3:
             arg_2_value = int(tokens[2]) if tokens[2].lstrip("-").isnumeric() else state[tokens[2]]
@@ -42,29 +41,45 @@ def evaluate_lines(lines: list[str], model_nr: int):
     return state
 
 
+def optimize_and_tokenize_inputs(lines: list[str]):
+    tokenized_inputs = []
+
+    for line in lines:
+        tokens = line.split()
+
+        # Remove useless input
+        if tokens[0] == "div" and tokens[2] == "1":
+            continue
+        else:
+            tokenized_inputs.append(tokens)
+
+    return tokenized_inputs
+
+
 if __name__ == "__main__":
     with open("d24_input.txt") as f:
         lines = f.readlines()
 
+    tokenized_inputs = optimize_and_tokenize_inputs(lines)
+    test_count = 0
     model_nr = 99999999999999
 
-    while True:
-        # print(f"Evaluating {model_nr}.")
+    while model_nr >= 11111111111111:
 
-        state = evaluate_lines(lines, model_nr)
+        if "0" in str(model_nr):
+            model_nr -= 1
+            continue
+
+        test_count += 1
+        if test_count % 10000 == 0:
+            print(f"Evaluating {model_nr}.")
+
+        state = evaluate_inputs(tokenized_inputs, model_nr)
         if state["z"] == 1:
             print(f"{model_nr} is largest valid model nr.")
             break
 
         model_nr -= 1
-
-        if model_nr % 10 == 0:
-            model_nr -= 1
-
         assert model_nr > 0
 
     pass
-# en dict som håller global state.
-# sedan utvärdera rad för rad och stoppa in global state.
-# stoppa in model_nr också så att man kan populera inp operationen.
-# parsa strängen med split eller nåt.
