78b1238a73f046cde1f40c8c9bc9e41a1cca60aa
Johan Rex <52hands@gmail.com>
2022-12-08 20:07:36 +0100

    8

--- 2022/8/8.py
+++ 2022/8/8.py
@@ -1,6 +1,4 @@
 import numpy as np
-from enum import Enum
-import sys
 
 UP = 0
 DOWN = 1
@@ -8,16 +6,14 @@ LEFT = 2
 RIGHT = 3
 
 
-# filename = "8/input"
-filename = "8/example"
+filename = "8/input"
+# filename = "8/example"
 lines = []
 with open(filename) as f:
     for line in f:
         lines.append([*(line.strip())])
 
 arr_grid = np.array(lines, np.int32)
-print(arr_grid.shape)
-
 
 h = arr_grid.shape[0]
 w = arr_grid.shape[1]
@@ -25,9 +21,6 @@ w = arr_grid.shape[1]
 # Make 3d array. Add one dimention to the existing shape.
 arr_visible = np.empty((4, h, w), dtype=bool)
 arr_visible[::] = True
-
-print(arr_grid)
-print(arr_visible)
 
 direction = LEFT
 for row in range(h):
@@ -90,3 +83,60 @@ print(
 )
 
 # -----------------------------------------
+
+arr_scenic_dist = np.empty((4, h, w), int)
+arr_scenic_dist[::] = 0
+
+for row in range(h):
+    for col in range(w):
+        curr = arr_grid[row, col]
+
+        # LEFT
+        scenic_dist = 0
+        for other_col in range(col + 1, w):
+            other = arr_grid[row, other_col]
+            if other > curr:
+                break
+            else:
+                scenic_dist += 1
+                if other == curr:
+                    break
+        arr_scenic_dist[LEFT, row, col] = scenic_dist
+
+        # RIGHT
+        scenic_dist = 0
+        for other_col in reversed(range(0, col)):
+            other = arr_grid[row, other_col]
+            if other > curr:
+                break
+            else:
+                scenic_dist += 1
+                if other == curr:
+                    break
+        arr_scenic_dist[RIGHT, row, col] = scenic_dist
+
+        # DOWN
+        scenic_dist = 0
+        for other_row in range(row + 1, h):
+            other = arr_grid[other_row, col]
+            if other > curr:
+                break
+            else:
+                scenic_dist += 1
+                if other == curr:
+                    break
+        arr_scenic_dist[DOWN, row, col] = scenic_dist
+
+        # UP
+        scenic_dist = 0
+        for other_row in reversed(range(0, row)):
+            other = arr_grid[other_row, col]
+            if other > curr:
+                break
+            else:
+                scenic_dist += 1
+                if other == curr:
+                    break
+        arr_scenic_dist[UP, row, col] = scenic_dist
+
+print("Part2:", np.max(arr_scenic_dist[DOWN] * arr_scenic_dist[UP] * arr_scenic_dist[LEFT] * arr_scenic_dist[RIGHT]))
