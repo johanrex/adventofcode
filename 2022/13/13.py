2af4cc380f9bbb934cb7019d679a7d54a021a079
Johan Rex <52hands@gmail.com>
2022-12-13 22:41:30 +0100

    13

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -1,5 +1,6 @@
+from functools import cmp_to_key
 import json
-import copy
+from math import prod
 
 
 def cmp(a, b) -> int:
@@ -11,7 +12,6 @@ def cmp(a, b) -> int:
             return 1
         else:
             return 0
-
     elif type(a) == list and type(b) == list:
         l = max(len(a), len(b))
         for i in range(l):
@@ -23,7 +23,6 @@ def cmp(a, b) -> int:
                 res = cmp(a[i], b[i])
                 if res != 0:
                     return res
-
     elif type(a) == int and type(b) == list:
         return cmp([a], b)
     elif type(a) == list and type(b) == int:
@@ -32,23 +31,6 @@ def cmp(a, b) -> int:
         raise Exception("nope")
 
     return 0
-
-
-def flatten(lst: list) -> list:
-    new = []
-    for item in lst:
-        if type(item) == list:
-            new.extend(flatten(item))
-        else:
-            new.append(item)
-    return new
-
-
-def listify(old: list[int], idx):
-    assert type(old[idx]) == int
-    new = copy.deepcopy(old)
-    new[idx] = [new[idx]]
-    return new
 
 
 # filename = "13/example"
@@ -64,15 +46,10 @@ for i in range(0, len(lines), 3):
     a = lines[i]
     b = lines[i + 1]
 
-    # print(a)
-    # print(b)
-
     a = json.loads(a)
     b = json.loads(b)
 
     result = cmp(a, b)
-    print(result < 0)
-
     if result < 0:
         correct_pair_idx.append(pair_idx)
 
@@ -82,12 +59,19 @@ part1 = sum(correct_pair_idx)
 assert part1 == 6046
 print("Part1:", part1)
 
-# -----------------------------------
-
-
 lines.append("[[2]]")
 lines.append("[[6]]")
 lists = [json.loads(line) for line in lines if line != ""]
 
-
 sorted_lists = sorted(lists, key=cmp_to_key(cmp))
+
+idxs = []
+dividers = {"[[2]]", "[[6]]"}
+for i, lst in enumerate(sorted_lists):
+    current = json.dumps(lst)
+    if current in dividers:
+        idxs.append(i + 1)
+
+part2 = prod(idxs)
+assert part2 == 21423
+print("Part2:", part2)
