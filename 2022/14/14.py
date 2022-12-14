f9d25ed41684edf6c13b8924c1a47c581b2747cf
Johan Rex <52hands@gmail.com>
2022-12-14 22:49:22 +0100

    14p1

--- 2022/14/14.py
+++ 2022/14/14.py
@@ -6,7 +6,7 @@ import bisect
 """
 
 
-def print_current(blocked):
+def print_current(blocked, sands):
     min_x = blocked[0][0]
     max_x = blocked[-1][0]
 
@@ -22,10 +22,17 @@ def print_current(blocked):
     for _ in range(y_count):
         lines.append(["."] * x_count)
 
+    lines[0][500 - min_x + 1] = "+"
+
     for x, y in blocked:
         norm_y = y - min_y
         norm_x = x - min_x + 1
         lines[norm_y][norm_x] = "#"
+
+    for x, y in sands:
+        norm_y = y - min_y
+        norm_x = x - min_x + 1
+        lines[norm_y][norm_x] = "o"
 
     for i, line in enumerate(lines):
         print(i, " ", "".join(line))
@@ -37,7 +44,7 @@ def parse_input(filename):
     with open(filename) as f:
         prev_x = prev_y = None
         for line in f:
-            print("Parsing line: ", line)
+            # print("Parsing line: ", line)
             coords = line.strip().split(" -> ")
 
             for i in range(1, len(coords)):
@@ -64,12 +71,62 @@ def parse_input(filename):
     return blocked
 
 
-filename = "14/example"
+def index(lst, x):
+    i = bisect.bisect_left(lst, x)
+    if i != len(lst) and lst[i] == x:
+        return i
+    return None
+
+
+def is_free(coord, blocked, sands):
+    if index(blocked, coord) is None and index(sands, coord) is None:
+        return True
+    else:
+        return False
+
+
+def is_block_below(x, y, blocked):
+    below = next((coord for coord in blocked if coord[0] == x and coord[1] > y), None)
+    return below is not None
+
+
+def add_sand(blocked: list[tuple[int, int]], sands: list[tuple[int, int]]):
+    curr_x, curr_y = (500, 0)
+    overflow = False
+    at_rest = False
+    while not at_rest:
+        # Do we get overflow?
+        if not is_block_below(curr_x, curr_y, blocked):
+            overflow = True
+            break
+
+        # Can move down?
+        if is_free((curr_x, curr_y + 1), blocked, sands):
+            curr_y += 1
+        else:
+            if is_free((curr_x - 1, curr_y + 1), blocked, sands):
+                curr_x -= 1
+                curr_y += 1
+            elif is_free((curr_x + 1, curr_y + 1), blocked, sands):
+                curr_x += 1
+                curr_y += 1
+            else:
+                bisect.insort(sands, (curr_x, curr_y))
+                # print_current(blocked, sands)
+                at_rest = True
+
+    return not overflow
+
+
+filename = "14/input"
 blocked = parse_input(filename)
 
-print("Blocked coords:")
+# print("Blocked coords:")
 print(blocked)
 
-print_current(blocked)
+sands = []
+while add_sand(blocked, sands):
+    pass
 
-print("Done")
+sands_at_rest = len(sands)
+print("Part1:", sands_at_rest)  # 808
