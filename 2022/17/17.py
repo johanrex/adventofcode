302128b34276c1e7fe5cc77a5e5111f3bdec875e
Johan Rex <52hands@gmail.com>
2022-12-17 14:31:45 +0100

    17

--- 2022/17/17.py
+++ 2022/17/17.py
@@ -62,6 +62,7 @@ def put_pattern_at(cave, pat, x_left, y_bottom):
 
 def can_move_in_direction(cave, pat, x_pat_left_src, y_pat_bottom_src, direction):
     x_offset = 0
+    y_offset = 0
     if direction == Direction.LEFT:
         x_offset = -1
         if x_pat_left_src <= 0:
@@ -70,12 +71,16 @@ def can_move_in_direction(cave, pat, x_pat_left_src, y_pat_bottom_src, direction
         x_offset = 1
         if x_pat_left_src + len(pat[0]) >= len(cave[0]):
             return False
+    elif direction == Direction.DOWN:
+        y_offset = 1
+        if y_pat_bottom_src + len(pat) >= len(cave):
+            return False
 
     pat_height = len(pat)
     pat_width = len(pat[0])
 
     pat_y = 0
-    for curr_y in range(y_pat_bottom_src - pat_height + 1, y_pat_bottom_src + 1):
+    for curr_y in range(y_pat_bottom_src + y_offset - pat_height + 1, y_pat_bottom_src + 1 + y_offset):
 
         pat_x = 0
         for curr_x in range(x_pat_left_src + x_offset, x_pat_left_src + pat_width + x_offset):
@@ -96,7 +101,7 @@ def can_move_down(cave, pat, x_pat_left_src, y_pat_bottom_src):
 
 filename = "17/example"
 with open(filename) as f:
-    jet = list(f.read().strip())
+    jets = list(f.read().strip())
 
 patterns = [
     [[*"@@@@"]],
@@ -110,14 +115,9 @@ cave: list[list[str]] = []
 pat_idx = 0
 jet_idx = 0
 
-# for pat in patterns:
-#     print_pattern(pat)
+nr_of_rocks = 2
 
-# extend_cave(cave, 3)
-# print_cave(cave)
-# print(count_empty_lines_from_top(cave))
-
-for i in range(1):
+for rock_idx in range(nr_of_rocks):
     empty_lines = count_empty_lines_from_top(cave)
     lines_to_extend = 3 - empty_lines
     assert lines_to_extend >= 0
@@ -130,25 +130,31 @@ for i in range(1):
     pat_left_x = 2
     pat_bottom_y = pat_height - 1
     put_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+    print(f"Rock {rock_idx+1} spawned.")
     print_cave(cave)
 
-    while can_move_in_direction(cave, pat, pat_left_x, pat_bottom_y, Direction.LEFT):
-        remove_pattern_at(cave, pat, pat_left_x, pat_height - 1)
-        pat_left_x -= 1
-        put_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+    is_falling = True
+    while is_falling:
+        jet = jets[jet_idx]
+        if jet == ">":
+            direction = Direction.RIGHT
+            x_offset = 1
+        else:
+            direction = Direction.LEFT
+            x_offset = -1
+
+        if can_move_in_direction(cave, pat, pat_left_x, pat_bottom_y, direction):
+            remove_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+            pat_left_x += x_offset
+            put_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+
+        if can_move_in_direction(cave, pat, pat_left_x, pat_bottom_y, Direction.DOWN):
+            remove_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+            pat_bottom_y += 1
+            put_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
+        else:
+            is_falling = False
+
         print_cave(cave)
-
-    while can_move_in_direction(cave, pat, pat_left_x, pat_bottom_y, Direction.RIGHT):
-        remove_pattern_at(cave, pat, pat_left_x, pat_height - 1)
-        pat_left_x += 1
-        put_pattern_at(cave, pat, pat_left_x, pat_bottom_y)
-        print_cave(cave)
-
-    # Bottom left pos
-    # pattern_pos =
-
-    # is_at_rest()
-
-    # "Each rock appears so that its left edge is two units away from the left wall and its bottom edge is three units above the highest rock in the room (or the floor, if there isn't one)."
-
+        jet_idx = (jet_idx + 1) % len(jets)
     pat_idx = (pat_idx + 1) % len(patterns)
