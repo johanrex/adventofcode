2a87e380b4441045be7b6c54da6b77201775e955
Johan Rex <52hands@gmail.com>
2022-12-18 15:10:52 +0100

    18

---
+++ 2022/18/18.py
@@ -0,0 +1,134 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+import sys
+
+
+class Direction(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+    UP = auto()
+    DOWN = auto()
+    FRONT = auto()
+    BACK = auto()
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+    z: int
+
+
+def parse(filename):
+    points = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            vals = list(map(int, line.split(",")))
+            c = Point(vals[0], vals[1], vals[2])
+            points.append(c)
+    return points
+
+
+def get_visible_sides(points):
+
+    side_cnt = 0
+
+    for p1 in points:
+        # left
+        if next((p2 for p2 in points if p1.x - 1 == p2.x and p1.y == p2.y and p1.z == p2.z), None) is None:
+            side_cnt += 1
+
+        # right
+        if next((p2 for p2 in points if p1.x + 1 == p2.x and p1.y == p2.y and p1.z == p2.z), None) is None:
+            side_cnt += 1
+
+        # up
+        if next((p2 for p2 in points if p1.x == p2.x and p1.y + 1 == p2.y and p1.z == p2.z), None) is None:
+            side_cnt += 1
+
+        # down
+        if next((p2 for p2 in points if p1.x == p2.x and p1.y - 1 == p2.y and p1.z == p2.z), None) is None:
+            side_cnt += 1
+
+        # front
+        if next((p2 for p2 in points if p1.x == p2.x and p1.y == p2.y and p1.z + 1 == p2.z), None) is None:
+            side_cnt += 1
+
+        # back
+        if next((p2 for p2 in points if p1.x == p2.x and p1.y == p2.y and p1.z - 1 == p2.z), None) is None:
+            side_cnt += 1
+
+    return side_cnt
+
+
+def get_enclosing_points(points):
+
+    min_x = min_y = min_z = sys.maxsize
+    max_x = max_y = max_z = -sys.maxsize
+
+    for p in points:
+        min_x = min(min_x, p.x)
+        min_y = min(min_y, p.y)
+        min_z = min(min_z, p.z)
+
+        max_x = max(max_x, p.x)
+        max_y = max(max_y, p.y)
+        max_z = max(max_z, p.z)
+
+    return Point(min_x - 1, min_y - 1, min_z - 1), Point(max_x + 1, max_y + 1, max_z + 1)
+
+
+def get_neighbors(p: Point, min_p: Point, max_p: Point) -> set[Point]:
+    ns = set()
+    if p.x - 1 >= min_p.x:
+        ns.add(Point(p.x - 1, p.y, p.z))
+    if p.x + 1 <= max_p.x:
+        ns.add(Point(p.x + 1, p.y, p.z))
+    if p.y - 1 >= min_p.y:
+        ns.add(Point(p.x, p.y - 1, p.z))
+    if p.y + 1 <= max_p.y:
+        ns.add(Point(p.x, p.y + 1, p.z))
+    if p.z - 1 >= min_p.z:
+        ns.add(Point(p.x, p.y, p.z - 1))
+    if p.z + 1 <= max_p.z:
+        ns.add(Point(p.x, p.y, p.z + 1))
+    return ns
+
+
+def is_in_bounds(p: Point, min_p: Point, max_p: Point) -> bool:
+    return min_p.x <= p.x <= max_p.x and min_p.y <= p.y <= max_p.y and min_p.z <= p.z <= max_p.z
+
+
+def flood_fill(min_p, max_p, points):
+    q = [min_p]
+    visited = set()
+    surface_pairs = set()
+    while len(q) > 0:
+        p = q.pop()
+        visited.add(p)
+        visit_candidates = get_neighbors(p, min_p, max_p)
+
+        for n in visit_candidates:
+            if n in points:
+                surface_pairs.add((p, n))
+            else:
+                if n not in visited:
+                    q.append(n)
+
+    return len(surface_pairs)
+
+
+# filename = "18/example"
+filename = "18/input"
+points = parse(filename)
+
+sides = get_visible_sides(points)
+assert sides == 4504
+print("Part1:", sides)
+
+min_p, max_p = get_enclosing_points(points)
+
+surfaces = flood_fill(min_p, max_p, points)
+assert surfaces == 2556
+print("Part2:", surfaces)
