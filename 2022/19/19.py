6eb8ee2374c7be8b47730560509dfb00d9e7150b
Johan Rex <52hands@gmail.com>
2022-12-29 17:46:57 +0100

    19

--- 2022/19/19.py
+++ 2022/19/19.py
@@ -4,6 +4,7 @@ from functools import cache
 import re
 from typing import Self
 import json
+import time
 
 pat = re.compile(
     r"Blueprint (?P<blueprint_id>\d+): Each ore robot costs (?P<ore_robot_cost_ore>\d+) ore. Each clay robot costs (?P<clay_robot_cost_ore>\d+) ore. Each obsidian robot costs (?P<obsidian_robot_cost_ore>\d+) ore and (?P<obsidian_robot_cost_clay>\d+) clay. Each geode robot costs (?P<geode_robot_cost_ore>\d+) ore and (?P<geode_robot_cost_obsidian>\d+) obsidian."
@@ -21,6 +22,16 @@ class Material(IntEnum):
 class Blueprint:
     blueprint_id: int
     factory_costs: dict[Material, dict[Material, int]]
+
+    def get_max_material_cost(self) -> dict[Material, int]:
+        max_material_cost = {m: 0 for m in Material}
+
+        for _, material_costs in self.factory_costs.items():
+            for material, cost in material_costs.items():
+                if max_material_cost[material] < cost:
+                    max_material_cost[material] = cost
+
+        return max_material_cost
 
     def __repr__(self) -> str:
         return json.dumps(self.__dict__)
@@ -66,7 +77,8 @@ class State:
         return msg
 
 
-def can_produce(state: State, bp: Blueprint, material_to_produce: Material) -> bool:
+# @cache
+def can_produce_robot(state: State, bp: Blueprint, material_to_produce: Material) -> bool:
     materials_needed = bp.factory_costs[material_to_produce]
 
     for material in materials_needed:
@@ -89,35 +101,18 @@ def add_new_robot(state: State, bp: Blueprint, material_to_produce: Material) ->
     return new_state
 
 
-@cache
-def get_next_possible_states(old_state: State, bp: Blueprint, current_time: int, stop_time: int) -> set[State]:
-    possible_next_states = set()
+def get_initial_state(bp_id: int) -> State:
+    state = State()
+    state.blueprint_id = bp_id
+    state.material_count = dict()
+    state.robot_count = dict()
 
-    if can_produce(old_state, bp, Material.GEODE):
-        new_state = add_new_robot(old_state, bp, Material.GEODE)
-        possible_next_states.add(new_state)
-    else:
-        if can_produce(old_state, bp, Material.OBSIDIAN):
-            new_state = add_new_robot(old_state, bp, Material.OBSIDIAN)
-            possible_next_states.add(new_state)
+    for material in Material:
+        state.material_count[material] = 0
+        state.robot_count[material] = 0
 
-        if can_produce(old_state, bp, Material.CLAY):
-            new_state = add_new_robot(old_state, bp, Material.CLAY)
-            possible_next_states.add(new_state)
-
-        if can_produce(old_state, bp, Material.ORE):
-            new_state = add_new_robot(old_state, bp, Material.ORE)
-            possible_next_states.add(new_state)
-
-        # Add the "do nothing" state
-        possible_next_states.add(old_state.copy())
-
-    for new_state in possible_next_states:
-        # Add one material for each robot that existed at old state.
-        for material, robot_count in old_state.robot_count.items():
-            new_state.material_count[material] += robot_count
-
-    return possible_next_states
+    state.robot_count[Material.ORE] = 1
+    return state
 
 
 def parse(filename) -> list[Blueprint]:
@@ -161,18 +156,44 @@ def print_bp(bp: Blueprint):
     print(msg)
 
 
-def get_initial_state(bp_id: int) -> State:
-    state = State()
-    state.blueprint_id = bp_id
-    state.material_count = dict()
-    state.robot_count = dict()
+# @cache
+def get_next_possible_states(old_state: State, bp: Blueprint, current_time: int, stop_time: int) -> set[State]:
+    possible_next_states: set[State] = set()
 
-    for material in Material:
-        state.material_count[material] = 0
-        state.robot_count[material] = 0
+    if can_produce_robot(old_state, bp, Material.GEODE):
+        new_state = add_new_robot(old_state, bp, Material.GEODE)
+        possible_next_states.add(new_state)
+    else:
+        # TODO will produce more than theoretical maximum spend?
+        time_left = stop_time - current_time
 
-    state.robot_count[Material.ORE] = 1
-    return state
+        if ((old_state.robot_count[Material.OBSIDIAN] * time_left) < (max_material_costs[Material.OBSIDIAN] * time_left)) and can_produce_robot(
+            old_state, bp, Material.OBSIDIAN
+        ):
+            new_state = add_new_robot(old_state, bp, Material.OBSIDIAN)
+            possible_next_states.add(new_state)
+
+        if ((old_state.robot_count[Material.CLAY] * time_left) < (max_material_costs[Material.CLAY] * time_left)) and can_produce_robot(
+            old_state, bp, Material.CLAY
+        ):
+            new_state = add_new_robot(old_state, bp, Material.CLAY)
+            possible_next_states.add(new_state)
+
+        if ((old_state.robot_count[Material.ORE] * time_left) < (max_material_costs[Material.ORE] * time_left)) and can_produce_robot(
+            old_state, bp, Material.ORE
+        ):
+            new_state = add_new_robot(old_state, bp, Material.ORE)
+            possible_next_states.add(new_state)
+
+        # Add the "do nothing" state
+        possible_next_states.add(old_state.copy())
+
+    for new_state in possible_next_states:
+        # Add one material for each robot that existed at old state.
+        for material, robot_count in old_state.robot_count.items():
+            new_state.material_count[material] += robot_count
+
+    return possible_next_states
 
 
 filename = "19/example"
@@ -180,33 +201,44 @@ filename = "19/example"
 bps = parse(filename)
 
 
+sum_max_quality_levels = 0
+
 for bp in bps:
     stop_time = 24
 
+    bp_max_quality_level = 0
+
     prev_states: set[State] = set()
-    current_states: set[State]
+
+    max_material_costs = bp.get_max_material_cost()
 
     for current_time in range(stop_time):
+        start_time = time.perf_counter()
+        current_states: set[State] = set()
 
         if current_time == 0:
-            states: set[State] = set()
-            states.add(get_initial_state(bp.blueprint_id))
-            current_states = states
+            current_states.add(get_initial_state(bp.blueprint_id))
         else:
-            states = set()
             for prev_state in prev_states:
-                states |= get_next_possible_states(prev_state, bp, current_time, stop_time)
+                current_states |= get_next_possible_states(prev_state, bp, current_time, stop_time)
 
-            # tmp = sorted(states, key=lambda s: s.robot_count[Material.GEODE], reverse=True)
-            current_states = states
+        max_geodes = sorted([s.material_count[Material.GEODE] for s in current_states], reverse=True)[0]
 
-        # Log states
-        print(f"Minute {current_time+1} elapsed. {len(current_states)} states.")
+        stop_time = time.perf_counter()
+
+        if bp_max_quality_level < (bp.blueprint_id * max_geodes):
+            bp_max_quality_level = bp.blueprint_id * max_geodes
+
+        print(
+            f"Minute {current_time+1} elapsed. Bp: {bp.blueprint_id}. States: {len(current_states)}. Max geodes: {max_geodes}. Max quality level: {bp_max_quality_level}. Speed: {len(current_states)/(stop_time-start_time):.2f} states/sec. Time: {(stop_time-start_time):.2f} sec."
+        )
+
         prev_states = current_states
+    sum_max_quality_levels += bp_max_quality_level
 
     pass
-    # print_bp(bp)
 
+print("Part1:", sum_max_quality_levels)
 
 # pip install pulp
 # from pulp import LpMaximize, LpProblem, LpStatus, LpVariable
