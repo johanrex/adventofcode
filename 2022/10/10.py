823fc1ee101fb031810829e0f23e2bbee31d8bf9
Johan Rex <52hands@gmail.com>
2023-07-23 17:12:42 +0200

    10

--- 2022/10/10.py
+++ 2022/10/10.py
@@ -1,59 +1,66 @@
-from dataclasses import dataclass
+#filename = "10/example"
+filename = "10/input"
 
 
-@dataclass
-class State:
-    cycle_nr: int
-    reg_x_val: int
+def parse(filename):
+    curr_cycle = 1
+    x_reg = 1
+    cycle_x = {}
+
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line == "noop":
+                cycle_x[curr_cycle] = x_reg
+                curr_cycle += 1
+            else:
+                instr, val = line.split()
+                if instr == "addx":
+                    cycle_x[curr_cycle] = x_reg
+                    cycle_x[curr_cycle + 1] = x_reg
+
+                    x_reg += int(val)
+                    curr_cycle += 2
+                else:
+                    raise ValueError(f"Unknown instruction {instr}")
+
+    return cycle_x
 
 
-def op_noop(current_state: State) -> State:
-    return State(current_state.cycle_nr + 1, current_state.reg_x_val)
+def part1(mapper):
+    sum_of_signal_strengths = 0
+    cycles_to_check = [x for x in range(20, 220 + 1, 40)]
+
+    for c in cycles_to_check:
+        sum_of_signal_strengths += c * mapper[c]
+
+    return sum_of_signal_strengths
 
 
-def op_addx(current_state: State, value: int) -> State:
-    return State(current_state.cycle_nr + 2, current_state.reg_x_val + value)
+def part2(mapper):
+    crt_width = 40
+    crt_height = 6
+
+    crt = ["."] * (crt_width * crt_height)
+
+    def print_crt(crt):
+        start = 0
+        end = crt_width
+
+        while end <= len(crt):
+            print("".join(crt[start:end]))
+            start += crt_width
+            end += crt_width
+
+    for cycle in range(1, len(crt) + 1):
+        x_val = mapper[cycle]
+        cycle_mod = cycle % crt_width
+        if x_val <= cycle_mod <= x_val + 2:
+            crt[cycle - 1] = "#"
+
+    print_crt(crt)
 
 
-filename = "10/example"
-
-part1_sum_of_signal_strengths = 0
-cycles_to_check = [x for x in range(20, 220 + 1, 40)]
-
-current_state = State(0, 1)
-with open(filename) as f:
-    for line in f:
-        if len(cycles_to_check) == 0:
-            break
-
-        line = line.strip()
-        old_state = current_state
-        if line == "noop":
-            current_state = op_noop(old_state)
-        elif line.startswith("addx"):
-            instr, val = line.split()
-            current_state = op_addx(old_state, int(val))
-
-        cycle = cycles_to_check[0]
-
-        val = None
-        if old_state.cycle_nr <= cycle <= current_state.cycle_nr:
-            val = old_state.reg_x_val
-
-        if val is not None:
-            signal_strength = cycle * val
-            part1_sum_of_signal_strengths += signal_strength
-            cycles_to_check.pop(0)
-            # print(signal_strength)
-            # print(part1_sum_of_signal_strengths)
-
-print("Part1:", part1_sum_of_signal_strengths)
-
-
-def print_crt(crt):
-
-    for line in crt:
-        print("".join(line))
-
-
-crt = [["."] * 40] * 6
+cycle_x_mapper = parse(filename)
+print("Part1:", part1(cycle_x_mapper))
+part2(cycle_x_mapper)
