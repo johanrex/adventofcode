31cd9d8d51c9810f5c4d0fa2c9d16462d13c2733
Johan Rex <52hands@gmail.com>
2023-12-03 08:07:23 +0100

    setup

--- 2023/setup_day.ps1
+++ 2023/setup_day.ps1
@@ -6,10 +6,20 @@ if (-not (Test-Path $day)) {
     New-Item -Path ".\$day\$day.py" -ItemType File # create the py file
     New-Item -Path ".\$day\example" -ItemType File # create the example file
 
+    Write-Host "Created files for day $day"
+}
+else {
+    Write-Host "Files for day $day already exist"
+}
+
+
+# should we download the input?
+if (-not (Test-Path $day/input)) {
+
     # sleep until 6am to download the input
     while ((Get-Date).Hour -lt 6) {
         Start-Sleep -Seconds 1
-    }
+    }    
 
     # read the cookie file
     $json = Get-Content -Path "cookie.json" -Raw | ConvertFrom-Json
@@ -18,8 +28,8 @@ if (-not (Test-Path $day)) {
     $url = "https://adventofcode.com/2023/day/$day/input"
     curl --cookie "session=$session_cookie" $url > $day/input
 
-    Write-Host "Created files for day $day"
+    Write-Host "Downloaded input for day $day"
 }
 else {
-    Write-Host "Files for day $day already exist"
+    Write-Host "Input for day $day already exists"
 }
