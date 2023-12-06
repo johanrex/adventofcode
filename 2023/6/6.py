804dd341e4321fea7b856583b005ba8b25c6566c
Johan Rex <52hands@gmail.com>
2023-12-06 06:30:58 +0100

    6

--- 2023/6/6.py
+++ 2023/6/6.py
@@ -25,13 +25,11 @@ def parse(filename):
 def part1(times, distances):
     record_cnts = []
     for time, max_distance in zip(times, distances):
-        # print(time, max_distance)
         record_cnt = 0
         for t in range(time + 1):
             velocity = t
             remaining = time - t
             distance = velocity * remaining
-            # print(t, distance)
             if distance > max_distance:
                 record_cnt += 1
         record_cnts.append(record_cnt)
@@ -41,15 +39,22 @@ def part1(times, distances):
 
 
 def part2(times, distances):
-    race_time = int("".join([str(t) for t in times]))
-    race_distance = int("".join([str(d) for d in distances]))
+    time = int("".join([str(t) for t in times]))
+    max_distance = int("".join([str(d) for d in distances]))
 
-    print(race_time)
-    print(race_distance)
+    record_cnt = 0
+    for t in range(time + 1):
+        velocity = t
+        remaining = time - t
+        distance = velocity * remaining
+        if distance > max_distance:
+            record_cnt += 1
+
+    print("Part 2:", record_cnt)
 
 
-filename = "6/example"
-# filename = "6/input"
+# filename = "6/example"
+filename = "6/input"
 
 times, distances = parse(filename)
 part1(times, distances)
