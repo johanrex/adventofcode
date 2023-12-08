c08d5ad800eddfc760fdb2656faf904937838f0f
Johan Rex <52hands@gmail.com>
2023-12-08 21:00:41 +0100

    cleanup

--- 2023/setup_day.ps1
+++ 2023/setup_day.ps1
@@ -1,10 +1,11 @@
 
 $day = (Get-Date).Day
+$folder = "day$day"
 
-if (-not (Test-Path $day)) {
+if (-not (Test-Path $folder)) {
     mkdir $day
-    New-Item -Path ".\$day\$day.py" -ItemType File # create the py file
-    New-Item -Path ".\$day\example" -ItemType File # create the example file
+    New-Item -Path ".\$folder\$day.py" -ItemType File # create the py file
+    New-Item -Path ".\$folder\example" -ItemType File # create the example file
 
     Write-Host "Created files for day $day"
 }
@@ -12,9 +13,9 @@ else {
     Write-Host "Files for day $day already exist"
 }
 
-
+$input_file = "$folder/input"
 # should we download the input?
-if (-not (Test-Path $day/input)) {
+if (-not (Test-Path $input_file)) {
 
     # sleep until 6am to download the input
     while ((Get-Date).Hour -lt 6) {
@@ -26,7 +27,7 @@ if (-not (Test-Path $day/input)) {
     $session_cookie = $json."Content raw"
 
     $url = "https://adventofcode.com/2023/day/$day/input"
-    curl --cookie "session=$session_cookie" $url > $day/input
+    curl --cookie "session=$session_cookie" $url > $input_file
 
     Write-Host "Downloaded input for day $day"
 }
