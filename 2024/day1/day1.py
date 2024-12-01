6046a223b8c1a8ab98368688bb339517a338ed65
Johan Rex <52hands@gmail.com>
2024-12-01 06:44:09 +0100

    1

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -2,24 +2,26 @@ from collections import Counter
 
 
 def parse(filename):
-    res = []
+    lst1 = []
+    lst2 = []
+
     with open(filename) as f:
         for line in f.readlines():
             line = line.strip()
             toks = line.split()
             a = int(toks[0])
             b = int(toks[1])
-            res.append((a, b))
-    return res
 
-
-def part1(data):
-    lst1 = [item[0] for item in data]
-    lst2 = [item[1] for item in data]
+            lst1.append(a)
+            lst2.append(b)
 
     lst1.sort()
     lst2.sort()
 
+    return lst1, lst2
+
+
+def part1(lst1, lst2):
     diffs = [abs(lst2[i] - lst1[i]) for i in range(len(lst1))]
     s = sum(diffs)
 
@@ -27,10 +29,7 @@ def part1(data):
     print("Part 1:", s)
 
 
-def part2(data):
-    lst1 = [item[0] for item in data]
-    lst2 = [item[1] for item in data]
-
+def part2(lst1, lst2):
     c = Counter(lst2)
 
     s = 0
@@ -46,6 +45,6 @@ def part2(data):
 # filename = "day1/example"
 filename = "day1/input"
 
-data = parse(filename)
-part1(data)
-part2(data)
+lst1, lst2 = parse(filename)
+part1(lst1, lst2)
+part2(lst1, lst2)
