4384cfd50dd8485fbb44a351b819aa08841e023d
Johan Rex <52hands@gmail.com>
2024-12-02 06:26:42 +0100

    2

---
+++ 2024/day2/day2.py
@@ -0,0 +1,63 @@
+import math
+import re
+import copy
+from collections import Counter
+
+
+def parse_ints(filename: str):
+    nrs = []
+    with open(filename) as f:
+        for line in f:
+            nrs.append(list(map(int, re.findall(r"\d+", line))))
+    return nrs
+
+
+def is_safe(line):
+    diffs = []
+    for i in range(1, len(line)):
+        diff = line[i - 1] - line[i]
+        diffs.append(diff)
+
+    same_sign = all(d > 0 for d in diffs) or all(d < 0 for d in diffs)
+    nice_diff = all(1 <= abs(d) <= 3 for d in diffs)
+
+    safe = same_sign and nice_diff
+    return safe
+
+
+def part1(data):
+    safe_cnt = 0
+    for line in data:
+        if is_safe(line):
+            safe_cnt += 1
+
+    assert safe_cnt == 479
+    print("Part 1:", safe_cnt)
+
+
+def part2(data):
+    safe_cnt = 0
+    for line in data:
+        if is_safe(line):
+            safe_cnt += 1
+            continue
+        else:
+            for k in range(len(line)):
+                line_copy = copy.deepcopy(line)
+
+                line_copy.pop(k)
+
+                if is_safe(line_copy):
+                    safe_cnt += 1
+                    break
+
+    assert safe_cnt == 531
+    print("Part 2:", safe_cnt)
+
+
+# filename = "day2/example"
+filename = "day2/input"
+
+data = parse_ints(filename)
+part1(data)
+part2(data)
