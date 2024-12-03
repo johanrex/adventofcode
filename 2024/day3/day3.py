ac17b16e7487cd0a5c2ffc31ce4b6da63ad7932b
Johan Rex <52hands@gmail.com>
2024-12-03 16:42:05 +0100

    3

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -1,22 +1,22 @@
 import re
 
+re_instr = re.compile(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
 re_mul = re.compile(r"mul\((\d+),(\d+)\)")
-re_do = re.compile(r"do\(\)")
-re_dont = re.compile(r"don't\(\)")
 
 
 def part1(filename: str):
     with open(filename) as f:
         lines = f.readlines()
 
+    line = "".join(lines)
     s = 0
-    for line in lines:
-        muls = re_mul.findall(line)
 
-        for mul in muls:
-            a = int(mul[0])
-            b = int(mul[1])
-            s += a * b
+    muls = re_mul.findall(line)
+
+    for mul in muls:
+        a = int(mul[0])
+        b = int(mul[1])
+        s += a * b
 
     assert s == 161289189
     print("Part 1:", s)
@@ -35,32 +35,22 @@ def part2(filename):
 
     line = "".join(lines)
 
-    instr_list = []
-    instr_list.extend(get_all_matches(line, re_mul))
-    instr_list.extend(get_all_matches(line, re_do))
-    instr_list.extend(get_all_matches(line, re_dont))
-
-    # sort by position of match
-    instr_list.sort(key=lambda x: x[0])
-
-    mul_list = []
-    do = True
-    for instr in instr_list:
-        if instr[1].startswith("don't"):
-            do = False
-        elif instr[1].startswith("do"):
-            do = True
-        elif instr[1].startswith("mul"):
-            if do:
-                mul_list.append(instr[1])
+    instructions = re_instr.findall(line)
 
     s = 0
-    for mul in mul_list:
-        m = re_mul.match(mul)
-        a = int(m.group(1))
-        b = int(m.group(2))
+    do = True
+    for instr in instructions:
+        if instr.startswith("don't"):
+            do = False
+        elif instr.startswith("do"):
+            do = True
+        elif instr.startswith("mul"):
+            if do:
+                m = re_mul.match(instr)
+                a = int(m.group(1))
+                b = int(m.group(2))
 
-        s += a * b
+                s += a * b
 
     assert s == 83595109
     print("Part 2:", s)
