06783cf2aa646afcff62e7899d3538944634835d
Johan Rex <52hands@gmail.com>
2024-12-07 06:28:22 +0100

    7

--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -5,10 +5,17 @@ from collections import Counter
 import operator
 from itertools import product
 
-operators = [
+
+def concat(a: int, b: int) -> int:
+    return int(str(a) + str(b))
+
+
+operators_p1 = [
     operator.add,
     operator.mul,
 ]
+
+operators_p2 = [*operators_p1, concat]
 
 
 def parse(filename: str):
@@ -20,10 +27,10 @@ def parse(filename: str):
     return data
 
 
-def is_satisfiable(test_value, operands):
+def is_satisfiable(test_value, operands, list_of_operators):
     nr_of_operators = len(operands) - 1
 
-    for ops in product(operators, repeat=nr_of_operators):
+    for ops in product(list_of_operators, repeat=nr_of_operators):
         result = operands[0]
         for i in range(nr_of_operators):
             result = ops[i](result, operands[i + 1])
@@ -39,14 +46,22 @@ def part1(data):
         test_value = eq[0]
         operands = eq[1]
 
-        if is_satisfiable(test_value, operands):
+        if is_satisfiable(test_value, operands, operators_p1):
             s += test_value
 
     print("Part 1:", s)
 
 
 def part2(data):
-    print("Part 2:", -1)
+    s = 0
+    for eq in data:
+        test_value = eq[0]
+        operands = eq[1]
+
+        if is_satisfiable(test_value, operands, operators_p2):
+            s += test_value
+
+    print("Part 2:", s)
 
 
 # filename = "day7/example"
@@ -54,4 +69,4 @@ filename = "day7/input"
 
 data = parse(filename)
 part1(data)
-# part2(data)
+part2(data)
