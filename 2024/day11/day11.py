a510c61f7f646e721afa0798eedc7ee3c3337a97
Johan Rex <52hands@gmail.com>
2024-12-11 13:45:30 +0100

    11

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -4,8 +4,7 @@ import re
 import copy
 from collections import Counter
 
-
-memo = dict()
+counter = Counter()
 
 
 def parse(filename: str) -> tuple[int, ...]:
@@ -36,34 +35,6 @@ def apply_rules_p1(stones: tuple[int, ...]):
     return tuple(new_stones)
 
 
-def apply_rules_recursize(stones: tuple[int, ...]):
-    if stones in memo:
-        return memo[stones]
-
-    if len(stones) == 1:
-        stone = stones[0]
-
-        if stone == 0:  # rule 1
-            ans = (1,)
-        elif len(str(stone)) % 2 == 0:  # rule 2
-            stone = str(stone)
-            middle = len(stone) // 2
-            left = int(stone[:middle])
-            right = int(stone[middle:])
-            ans = left, right
-        else:  # rule 3
-            ans = (stone * 2024,)
-    else:
-        # split in half
-        middle = len(stones) // 2
-        left = stones[:middle]
-        right = stones[middle:]
-        ans = apply_rules_recursize(left) + apply_rules_recursize(right)
-
-    memo[stones] = ans
-    return ans
-
-
 def part1(stones: tuple[int, ...]):
     print("Initial state:")
     print(stones)
@@ -79,15 +50,41 @@ def part1(stones: tuple[int, ...]):
     print("Part 1:", ans)
 
 
+def apply_rules_p2(counter: Counter):
+    counter_before = copy.deepcopy(counter)
+
+    actual_stone_vals = [k for k, v in counter.items() if v > 0]
+    for stone_val in actual_stone_vals:
+        stone_count = counter_before[stone_val]
+        counter[stone_val] -= stone_count
+        if counter[stone_val] < 0:
+            counter[stone_val] = 0
+
+        if stone_val == 0:  # rule 1
+            counter[1] += stone_count
+        elif len(str(stone_val)) % 2 == 0:  # rule 2
+            stone_val = str(stone_val)
+            middle = len(stone_val) // 2
+            left = int(stone_val[:middle])
+            right = int(stone_val[middle:])
+
+            counter[left] += stone_count
+            counter[right] += stone_count
+        else:  # rule 3
+            counter[stone_val * 2024] += stone_count
+
+
 def part2(stones: tuple[int, ...]):
     print("Initial state:")
     print(stones)
+    for stone in stones:
+        counter[stone] += 1
 
     for i in range(75):
-        stones = apply_rules_recursize(stones)
-        print(f"After {i+1} blinks:", len(stones))
+        apply_rules_p2(counter)
 
-    ans = len(stones)
+    ans = sum(counter.values())
+    assert ans == 257246536026785
     print("Part 2:", ans)
 
 
