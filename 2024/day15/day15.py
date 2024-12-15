199a0d5a1af16bafe598e63a5475c53b7cefe30c
Johan Rex <52hands@gmail.com>
2024-12-15 09:46:06 +0100

    15

--- 2024/day15/day15.py
+++ 2024/day15/day15.py
@@ -149,11 +149,11 @@ def move(grid: Grid, robot_pos: Pos, d: Pos, steps_to_space: int):
     return robot_pos
 
 
-def sum_of_box_gps(grid):
+def sum_of_box_gps(grid, target="O"):
     sum = 0
     for r in range(grid.rows):
         for c in range(grid.cols):
-            if grid.get(r, c) == "O":
+            if grid.get(r, c) == target:
                 sum += r * 100 + c
     return sum
 
@@ -161,7 +161,6 @@ def sum_of_box_gps(grid):
 def part1(grid, robot_pos: Pos, movements):
     print("Initial state:")
     grid.print_grid()
-    print(robot_pos)
     print(movements)
 
     for m in movements:
@@ -179,13 +178,120 @@ def part1(grid, robot_pos: Pos, movements):
     print("Part 1:", ans)
 
 
-def part2(data):
-    print("Part 2:", -1)
+def create_wide_grid(grid: Grid) -> tuple[Grid, Pos]:
+    wgrid = Grid(grid.rows, grid.cols * 2)
+    robot_pos = None
+
+    for r in range(grid.rows):
+        for c in range(grid.cols):
+            val = grid.get(r, c)
+            if val == "@":
+                wgrid.set(r, c * 2, "@")
+                wgrid.set(r, c * 2 + 1, ".")
+
+                robot_pos = (r, c * 2)
+            elif val == "O":
+                wgrid.set(r, c * 2, "[")
+                wgrid.set(r, c * 2 + 1, "]")
+            else:
+                wgrid.set(r, c * 2, val)
+                wgrid.set(r, c * 2 + 1, val)
+
+    assert robot_pos is not None
+    return wgrid, robot_pos
+
+
+def find_affected_grid_positions(grid: Grid, robot_pos: Pos, d: Pos):
+    # find positions to push in the direction d.
+    # a box is two positions wide.
+
+    d_r, d_c = d
+
+    q = [robot_pos]
+    affected_grid_pos = set([robot_pos])
+
+    while len(q) > 0:
+        affected_grid_r, affected_grid_c = q.pop(0)
+        next_grid_r, next_grid_c = affected_grid_r + d_r, affected_grid_c + d_c
+        val = grid.get(next_grid_r, next_grid_c)
+
+        if val == "#":
+            # if we hit a wall we can't move anything.
+            # we indicate this by returning None
+            affected_grid_pos = None
+            break
+
+        if val == "[":
+            q.append((next_grid_r, next_grid_c))
+            q.append((next_grid_r, next_grid_c + 1))
+
+            affected_grid_pos.add((next_grid_r, next_grid_c))
+            affected_grid_pos.add((next_grid_r, next_grid_c + 1))
+        elif val == "]":
+            q.append((next_grid_r, next_grid_c))
+            q.append((next_grid_r, next_grid_c - 1))
+
+            affected_grid_pos.add((next_grid_r, next_grid_c - 1))
+            affected_grid_pos.add((next_grid_r, next_grid_c))
+
+    return affected_grid_pos
+
+
+def part2(grid: Grid, movements):
+    grid, robot_pos = create_wide_grid(grid)
+
+    print("Movements:")
+    print(movements)
+
+    print("Initial state:")
+    grid.print_grid()
+    print("")
+
+    for movement_idx, m in enumerate(movements):
+        d = DIR_MAP[m]
+        d_r, d_c = d
+        if d_c != 0:  # we can reuse function from part 1
+            steps_to_space = find_empty_space(grid, robot_pos, d)
+            if steps_to_space != -1:
+                robot_pos = move(grid, robot_pos, d, steps_to_space)
+        else:
+            # we are pushing up or down
+
+            # find boxes involved in push
+            affected_grid_pos = find_affected_grid_positions(grid, robot_pos, d)
+            if affected_grid_pos is None:
+                continue
+            else:
+                if d_r == -1:
+                    affected_grid_pos = sorted(affected_grid_pos, key=lambda x: x[0])
+                else:
+                    affected_grid_pos = sorted(affected_grid_pos, key=lambda x: x[0], reverse=True)
+
+                for src_pos in affected_grid_pos:
+                    src_r, src_c = src_pos
+                    dst_r, dst_c = src_r + d_r, src_c
+
+                    assert grid.get(dst_r, dst_c) == "."
+
+                    val_to_move = grid.get(src_r, src_c)
+                    grid.set(src_r, src_c, ".")
+                    grid.set(dst_r, dst_c, val_to_move)
+
+            robot_pos = (robot_pos[0] + d_r, robot_pos[1])
+
+        # print(f"Move {m}. ({movement_idx=})")
+        # grid.print_grid()
+        # print("")
+
+    ans = sum_of_box_gps(grid, target="[")
+
+    print("Part 2:", ans)
 
 
 # filename = "day15/example"
 filename = "day15/input"
 
+# grid, robot_pos, movements = parse(filename)
+# part1(grid, robot_pos, movements)
 grid, robot_pos, movements = parse(filename)
-part1(grid, robot_pos, movements)
-# part2(data)
+part2(grid, movements)
