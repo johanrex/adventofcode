473fbdd87999f56a29678d32cc6c693e8cb65c27
Johan Rex <52hands@gmail.com>
2024-12-16 16:56:42 +0100

    16

--- 2024/day16/day16.py
+++ 2024/day16/day16.py
@@ -53,35 +53,64 @@ def dijkstra(grid: Grid, start: Grid.Pos, end: Grid.Pos, cost: dict[str, int]) -
     visited = set()
 
     while pq:
-        acc_cost, pos, prev_direction, path = heapq.heappop(pq)
-        if pos in visited:
+        curr_cost, curr_pos, prev_d, path = heapq.heappop(pq)
+        if curr_pos in visited:
             continue
-        visited.add(pos)
+        visited.add(curr_pos)
 
-        path = path + [pos]
+        path = path + [curr_pos]
 
-        if pos == end:
-            return acc_cost, path
+        if curr_pos == end:
+            return curr_cost, path
 
-        for new_direction in [Grid.Pos(0, 1), Grid.Pos(0, -1), Grid.Pos(1, 0), Grid.Pos(-1, 0)]:
-            new_pos = pos + new_direction
+        for d in [EAST, WEST, NORTH, SOUTH]:
+            next_pos = curr_pos + d
 
-            cell_value = grid.get_by_pos(new_pos)
+            cell_value = grid.get_by_pos(next_pos)
             if cell_value == "#":
                 continue
 
-            if new_pos in visited:
+            if next_pos in visited:
                 continue
 
             step_cost = 1
 
-            if prev_direction.row != new_direction.row and prev_direction.col != new_direction.col:
+            if prev_d.row != d.row and prev_d.col != d.col:
                 step_cost += 1000
 
-            new_cost = acc_cost + step_cost
-            heapq.heappush(pq, (new_cost, new_pos, new_direction, path))
+            heapq.heappush(pq, (curr_cost + step_cost, next_pos, d, path))
 
     return -1, []  # return -1 and empty path if no path is found
+
+
+def dfs_all_paths(grid: Grid, start: Grid.Pos, end: Grid.Pos, lowest_cost: int) -> list[list[Grid.Pos]]:
+    def dfs(curr_pos: Grid.Pos, facing: Grid.Pos, path: list[Grid.Pos], curr_cost: int):
+        if curr_cost > lowest_cost:
+            return
+
+        if curr_pos == end:
+            all_paths_costs.append((curr_cost, path[:]))
+            return
+
+        for d in [EAST, WEST, NORTH, SOUTH]:
+            new_pos = curr_pos + d
+
+            if new_pos in path:
+                continue
+
+            if grid.get_by_pos(new_pos) == "#":
+                continue
+
+            step_cost = 1
+
+            if d != facing:
+                step_cost += 1000
+
+            dfs(new_pos, d, path + [new_pos], curr_cost + step_cost)
+
+    all_paths_costs = []
+    dfs(start, EAST, [start], 0)
+    return all_paths_costs
 
 
 def print_path(grid: Grid, path: list[Grid.Pos]):
@@ -113,20 +142,50 @@ def print_path(grid: Grid, path: list[Grid.Pos]):
         print(line)
 
 
-def part1(grid: Grid, start: Grid.Pos, end: Grid.Pos):
-    grid.print_grid()
-    cost, path = dijkstra(grid, start, end, {"turn_east": 2})
-    print("Part 1:", cost)
+def print_tiles(grid: Grid, paths: list[list[Grid.Pos]]):
+    grid_copy = grid.copy()
+
+    for path in paths:
+        for pos in path:
+            grid_copy.set_by_pos(pos, "O")
+
+    # draw glorious grid with path
+    for row in range(grid_copy.rows):
+        line = ""
+        for col in range(grid_copy.cols):
+            pos = Grid.Pos(row, col)
+            line += grid_copy.get_by_pos(pos)
+        print(line)
+
+
+def solve(grid: Grid, start: Grid.Pos, end: Grid.Pos):
+    # grid.print_grid()
+    lowest_cost, path = dijkstra(grid, start, end, {"turn_east": 2})
+    print("Part 1:", lowest_cost)
     # print_path(grid, path)
 
+    all_paths_costs = dfs_all_paths(grid, start, end, lowest_cost)
 
-def part2(grid: Grid, start: Grid.Pos, end: Grid.Pos):
-    print("Part 2:", -1)
+    all_paths_costs = sorted(all_paths_costs, key=lambda x: x[0])
+    lowest_cost = all_paths_costs[0][0]
+    print("Lowest cost:", lowest_cost)
+
+    all_lowest_cost_paths = [path for cost, path in all_paths_costs if cost == lowest_cost]
+
+    print_tiles(grid, all_lowest_cost_paths)
+
+    set_of_tiles = set()
+    for path in all_lowest_cost_paths:
+        for pos in path:
+            set_of_tiles.add(pos)
+
+    ans = len(set_of_tiles)
+
+    print("Part 2:", ans)
 
 
 # filename = "day16/example"
 filename = "day16/input"
 
 grid, start, end = parse(filename)
-part1(grid, start, end)
-# part2(grid)
+solve(grid, start, end)
