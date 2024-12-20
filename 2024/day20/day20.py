138eba8819a76d870d6da5ebf0ef10dec761ef06
Johan Rex <52hands@gmail.com>
2024-12-20 08:34:25 +0100

    20

--- 2024/day20/day20.py
+++ 2024/day20/day20.py
@@ -12,6 +12,8 @@ from collections import defaultdict
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from utils.grid import Grid
+# from tqdm import tqdm
+
 import utils.parse_utils as parse_utils
 
 EAST = Grid.Pos(0, 1)
@@ -76,17 +78,62 @@ def dijkstra(grid: Grid, start: Grid.Pos) -> dict[Grid.Pos, int]:
     return distances
 
 
-def part1(grid: Grid, start: Grid.Pos, end: Grid.Pos):
+def get_walls(grid: Grid) -> set[Grid.Pos]:
+    walls = set()
+    for row in range(grid.rows):
+        for col in range(grid.cols):
+            val = grid.get(row, col)
+            if val == "#":
+                walls.add(Grid.Pos(row, col))
+
+    return walls
+
+
+def fastest_time(grid: Grid, start: Grid.Pos, end: Grid.Pos):
     dists = dijkstra(grid, start)
-
     d = dists[end]
-    print(d)
-
-    print("Part 1:", -1)
+    return d
 
 
-filename = "day20/example"
-# filename = "day20/input"
+def part1(grid: Grid, start: Grid.Pos, end: Grid.Pos):
+    min_time = sys.maxsize
+    uncheated_time = fastest_time(grid, start, end)
+
+    time_save_counter = defaultdict(int)
+
+    walls = get_walls(grid)
+    # for wall in tqdm(walls, desc="Processing walls"):
+    for i, wall in enumerate(walls):
+        if i % 100 == 0:
+            print(f"Processing wall {i}/{len(walls)}")
+
+        assert grid.get_by_pos(wall) == "#"
+
+        # cheat
+        grid.set_by_pos(wall, ".")
+        curr_time = fastest_time(grid, start, end)
+        min_time = min(min_time, curr_time)
+
+        time_saved = uncheated_time - curr_time
+        time_save_counter[time_saved] += 1
+
+        # uncheat
+        grid.set_by_pos(wall, "#")
+
+    target_time_save = 100
+    assert target_time_save in time_save_counter
+
+    cheat_count = 0
+    for k, v in time_save_counter.items():
+        if k >= target_time_save:
+            cheat_count += v
+
+    assert cheat_count == 1524
+    print("Part 1:", cheat_count)
+
+
+# filename = "day20/example"
+filename = "day20/input"
 
 grid, start, end = parse(filename)
 part1(grid, start, end)
