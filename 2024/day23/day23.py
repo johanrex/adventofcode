5edda701f7f5174c9ec0229b9bb72050626982a2
Johan Rex <52hands@gmail.com>
2024-12-26 16:08:00 +0100

    23

--- 2024/day23/day23.py
+++ 2024/day23/day23.py
@@ -13,6 +13,23 @@ def parse(filename: str) -> Graph:
     return graph
 
 
+def get_cliques(graph: Graph) -> set[tuple[str]]:
+    def bron_kerbosch(r: set[str], p: set[str], x: set[str]):
+        if not p and not x:
+            cliques.add(tuple(sorted(r)))
+        for v in list(p):
+            r_v = r | {v}
+            p_v = p & graph[v]
+            x_v = x & graph[v]
+            bron_kerbosch(r_v, p_v, x_v)
+            p.remove(v)
+            x.add(v)
+
+    cliques = set()
+    bron_kerbosch(set(), set(graph.keys()), set())
+    return cliques
+
+
 def part1(graph: Graph):
     sets = set()
     ts = [node_name for node_name in graph.keys() if node_name.startswith("t")]
@@ -30,8 +47,21 @@ def part1(graph: Graph):
     print("Part 1:", ans)
 
 
+def part2(graph: Graph):
+    cliques = get_cliques(graph)
+    cs_list = list(cliques)
+    cs_list.sort(key=lambda x: len(x), reverse=True)
+    max_clique = cs_list[0]
+    ans = ",".join(max_clique)
+
+    assert ans == "bg,bu,ce,ga,hw,jw,nf,nt,ox,tj,uu,vk,wp"
+
+    print("Part 2:", ans)
+
+
 filename = "day23/example"
 filename = "day23/input"
 
 graph = parse(filename)
 part1(graph)
+part2(graph)
