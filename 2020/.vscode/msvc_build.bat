19f154c78114cc335d18cc9372d4e8f7ca6f3de9
Johan Rex <52hands@gmail.com>
2025-11-28 00:16:19 +0100

    ...

--- 2020/.vscode/msvc_build.bat
+++ 2020/.vscode/msvc_build.bat
@@ -1,3 +1,9 @@
 @echo off
+REM Skip env setup if already in a VS dev environment
+if defined VSCMD_VER goto build
+if defined VCINSTALLDIR goto build
+
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl.exe /std:c++20 /Zc:__cplusplus /Zi /Od /W4 /EHsc "%~1" /Fe:"%~dpn1.exe"
+
+:build
+cl.exe /std:c++latest /Zc:__cplusplus /Zi /Od /W4 /EHsc "%~1" /Fe:"%~dpn1.exe"
