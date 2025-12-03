d4508da6ce387c35a4846dba4aba8e0bcdd219da
Johan Rex <52hands@gmail.com>
2025-12-03 08:02:04 +0100

    3

--- 2025/day03/day03.py
+++ 2025/day03/day03.py
@@ -1,18 +1,3 @@
-from dataclasses import dataclass
-import time
-import math
-import re
-import copy
-from collections import Counter
-import sys
-import os
-from collections import defaultdict
-import itertools
-
-# silly python path manipulation
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-
 def parse(filename) -> list[int]:
     data = []
     with open(filename) as f:
@@ -26,26 +11,32 @@ def parse(filename) -> list[int]:
     return data
 
 
-def part1(data):
-    total = 0
+def find_joltage(data, size):
+    ans = 0
+
     for row in data:
-        max_1 = -1
-        max_1_idx = -1
-        for i in range(len(row) - 1):
-            if row[i] > max_1:
-                max_1 = row[i]
-                max_1_idx = i
+        tmp_size = size
+        batteries = []
+        max_battery_idx = -1
+        while tmp_size > 0:
+            max_battery = -1
 
-        max_2 = -1
-        for i in range(max_1_idx + 1, len(row)):
-            if row[i] > max_2:
-                max_2 = row[i]
+            for i in range(max_battery_idx + 1, len(row) - tmp_size + 1):
+                if row[i] > max_battery:
+                    max_battery = row[i]
+                    max_battery_idx = i
 
-        max_num = max_1 * 10 + max_2
-        print(max_num)
-        total += max_num
+            batteries.append(max_battery)
 
-    print("Part 1:", total)
+            tmp_size -= 1
+
+        tmp = [str(n) for n in batteries]
+        tmp = "".join(tmp)
+        tmp = int(tmp)
+        # print(f"Found {str(tmp)} joltage in bank.")
+        ans += tmp
+
+    return ans
 
 
 def part2(data):
@@ -56,5 +47,10 @@ filename = "day03/example"
 filename = "day03/input"
 
 data = parse(filename)
-part1(data)
-# part2(data)
+# ans = part1(data)
+
+p1 = find_joltage(data, 2)
+print("Part 1:", p1)
+
+p2 = find_joltage(data, 12)
+print("Part 2:", p2)
