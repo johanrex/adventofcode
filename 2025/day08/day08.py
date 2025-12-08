94750f1973767c5f27c2342759503fe43199b27c
Johan Rex <52hands@gmail.com>
2025-12-08 12:39:09 +0100

    d8

--- 2025/day08/day08.py
+++ 2025/day08/day08.py
@@ -23,18 +23,14 @@ def parse(filename: str) -> list[Point]:
     return points
 
 
-def dist(p1: Point, p2: Point):
-    return (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2
-
-
-def part1(points: list[Point]):
+def solve(points: list[Point]):
     dists = []
     for i in range(len(points)):
         p1 = points[i]
         for j in range(i + 1, len(points)):
             p2 = points[j]
 
-            d = dist(p1, p2)
+            d = (p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2
             dists.append((d, (p1, p2)))
 
     dists.sort()
@@ -45,70 +41,11 @@ def part1(points: list[Point]):
     else:  # is real input
         connection_cnt = 1000
 
-    # cliques, init with all points separate
-    cliques = []
-    for p in points:
-        c = set()
-        c.add(p)
-        cliques.append(c)
-
-    # make connections
-    for i in range(connection_cnt):
-        d, (p1, p2) = dists[i]
-
-        # find clique for p1
-        for j, c1 in enumerate(cliques):
-            if p1 in c1:
-                break
-
-        # find clique for p2
-        for k, c2 in enumerate(cliques):
-            if p2 in c2:
-                break
-
-        if j == k:
-            continue  # already in same clique. Nothing to do
-
-        min_idx = min(j, k)
-        max_idx = max(j, k)
-
-        # let's merge the cliques
-
-        # pop the later clique to keep indices valid
-        later_clique = cliques.pop(max_idx)
-
-        # merge into the other clique
-        cliques[min_idx] = cliques[min_idx] | later_clique
-
-        pass
-
-    sizes = [len(c) for c in cliques]
-    sizes.sort(reverse=True)
-    ans = math.prod(sizes[:3])
-
-    print("Part 1:", ans)
-
-
-def part2(points: list[Point]):
-    dists = []
-    for i in range(len(points)):
-        p1 = points[i]
-        for j in range(i + 1, len(points)):
-            p2 = points[j]
-
-            d = dist(p1, p2)
-            dists.append((d, (p1, p2)))
-
-    dists.sort()
+    p1_ans = 0
+    p2_ans = 0
 
     # cliques, init with all points separate
-    cliques = []
-    for p in points:
-        c = set()
-        c.add(p)
-        cliques.append(c)
-
-    x_prod = 0
+    cliques = [set([p]) for p in points]
 
     # make connections
     for i in range(len(dists)):
@@ -127,10 +64,10 @@ def part2(points: list[Point]):
         if j == k:
             continue  # already in same clique. Nothing to do
 
+        # we have two cliques that should be merged.
+
         min_idx = min(j, k)
         max_idx = max(j, k)
-
-        # let's merge the cliques
 
         # pop the later clique to keep indices valid
         later_clique = cliques.pop(max_idx)
@@ -138,18 +75,22 @@ def part2(points: list[Point]):
         # merge into the other clique
         cliques[min_idx] = cliques[min_idx] | later_clique
 
-        x_prod = p1.x * p2.x
-        pass
+        if i == connection_cnt:
+            sizes = [len(c) for c in cliques]
+            sizes.sort(reverse=True)
+            p1_ans = math.prod(sizes[:3])
 
-    sizes = [len(c) for c in cliques]
-    sizes.sort(reverse=True)
+        p2_ans = p1.x * p2.x
 
-    print("Part 2:", x_prod)
+    assert p1_ans == 97384
+    print("Part 1:", p1_ans)
+
+    assert p2_ans == 9003685096
+    print("Part 2:", p2_ans)
 
 
-filename = "day08/example"
+# filename = "day08/example"
 filename = "day08/input"
 
 points = parse(filename)
-part1(points)
-part2(points)
+solve(points)
